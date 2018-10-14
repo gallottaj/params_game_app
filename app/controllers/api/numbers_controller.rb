0ac3004ed79505458ce4fa868f7a0cff8a82cb27
Joe Gallotta <joeygallotta@gmail.com>
2018-10-14 10:26:56 -0500

    displays message if name begins with A

--- app/controllers/api/numbers_controller.rb
+++ app/controllers/api/numbers_controller.rb
@@ -2,6 +2,10 @@ class Api::NumbersController < ApplicationController
 
   def add_new_param
     @name = params[:name].upcase
+    #if name begins with A, add a message
+    if @name.start_with?("A")
+      @name = "Your name begins with A!"
+    end
     render "new_param.json.jbuilder"
   end
 end
