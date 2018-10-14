0d900f6476109795d57b730d5c9e59e212bb46ee
Joe Gallotta <joeygallotta@gmail.com>
2018-10-14 10:23:04 -0500

    displays name in caps

--- app/controllers/api/numbers_controller.rb
+++ app/controllers/api/numbers_controller.rb
@@ -1,7 +1,8 @@
 class Api::NumbersController < ApplicationController
 
   def add_new_param
-    # @name = params [:name]
+    @name = params[:name]
     render "new_param.json.jbuilder"
   end
 end
+  
