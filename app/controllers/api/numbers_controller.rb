18cbb806cbb63e81df3cc9987fe2b471e5553b84
Joe Gallotta <joeygallotta@gmail.com>
2018-10-14 09:51:05 -0500

    successful route

--- app/controllers/api/numbers_controller.rb
+++ app/controllers/api/numbers_controller.rb
@@ -1,2 +1,7 @@
 class Api::NumbersController < ApplicationController
+
+  def add_new_param
+    # @name = params [:name]
+    render "new_param.json.jbuilder"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
+  namespace :api do
+    get '/add_params' => 'numbers#add_new_param'
+   end
 end
+
+
+
